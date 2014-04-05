c733706d9c9972ec74a6859eeedce5fd3b14293c
ShivamMistry <admin@shivam-mistry.co.uk>
2014-04-05 18:50:34 +0100

    adding longopts

--- src/main/java/com/speed/ob/Obfuscator.java
+++ src/main/java/com/speed/ob/Obfuscator.java
@@ -8,13 +8,13 @@ import org.apache.commons.cli.*;
 public class Obfuscator {
     public static void main(String[] args) {
         Options options = new Options();
-        options.addOption("a", false, "enable all obfuscations");
-        options.addOption("s", false, "enable string obfuscation");
-        options.addOption("l", false, "enable lexical obfuscation");
-        options.addOption("cf", false, "enable control flow obfuscation");
-        options.addOption("c", true, "use <arg> as a config file");
-        options.addOption("f", true, "obfuscate file <arg>");
-        options.addOption("o", true, "output obfuscated file(s) to <arg>");
+        options.addOption("a", "all", false, "enable all obfuscations");
+        options.addOption("s", "strings", false, "enable string obfuscation");
+        options.addOption("l", "lexical", false, "enable lexical obfuscation");
+        options.addOption("c", "control-flow", false, "enable control flow obfuscation");
+        options.addOption("C", "config", true, "use <arg> as a config file");
+        options.addOption("f", "file", true, "obfuscate file <arg>");
+        options.addOption("o", "out", true, "output obfuscated file(s) to <arg>");
         CommandLineParser parser = new GnuParser();
         HelpFormatter formatter = new HelpFormatter();
         try {
